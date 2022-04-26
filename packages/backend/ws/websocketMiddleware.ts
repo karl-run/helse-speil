05e2947af614a583f7dd5775281163bbd52b3d24
Sindre Broch <sindre.broch@nav.no>
2022-04-26 17:10:13 +0200

    Legger til logging

--- packages/backend/ws/websocketMiddleware.ts
+++ packages/backend/ws/websocketMiddleware.ts
@@ -1,10 +1,11 @@
-import config from '../config';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import logger from '../logging';
 
 const baseUrl = process.env.NODE_ENV === 'development' ? 'ws://localhost:9001' : 'ws://spesialist';
 
 export default () => ({
     getWebsocketProxy: () => {
+        logger.info('Setting up websocket proxy');
         return createProxyMiddleware({
             target: baseUrl,
             ws: true,
