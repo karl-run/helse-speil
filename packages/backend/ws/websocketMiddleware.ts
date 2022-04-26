4896ce26ac99c46ae7dc59475566d6ed2fbac16a
Sindre Broch <sindre.broch@nav.no>
2022-04-26 16:51:02 +0200

    Bruk ws:// mot spesialist

--- packages/backend/ws/websocketMiddleware.ts
+++ packages/backend/ws/websocketMiddleware.ts
@@ -1,7 +1,7 @@
 import config from '../config';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'ws://localhost:9001' : 'wss://spesialist';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'ws://localhost:9001' : 'ws://spesialist';
 
 export default () => ({
     getWebsocketProxy: () => {
