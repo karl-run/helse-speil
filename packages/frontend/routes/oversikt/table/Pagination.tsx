bff9a85c74a2ab004dd1249f2a567ef428a3812d
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-11-23 17:47:25 +0100

    💄 Visuell indikasjon når oppgavelisten fortsetter under pagineringen
    Når det ikke er plass i høyden til alle 14 oppgaver, trengs det et
    visuelt hint om at det ligger flere oppgaver lenger ned.

--- packages/frontend/routes/oversikt/table/Pagination.tsx
+++ packages/frontend/routes/oversikt/table/Pagination.tsx
@@ -13,16 +13,24 @@ interface PaginationProps {
 }
 
 export const Pagination = ({ numberOfEntries, numberOfPages, currentPage, limit, setPage }: PaginationProps) => (
-    <div className={styles.Pagination}>
-        <NavPagination page={currentPage} onPageChange={setPage} count={numberOfPages} siblingCount={2} prevNextTexts />
-        {numberOfEntries > 0 && (
-            <p>
-                Viser {currentPage * limit - (limit - 1)} til{' '}
-                {currentPage === numberOfPages && numberOfEntries % limit !== 0
-                    ? currentPage * limit - (limit - (numberOfEntries % limit))
-                    : currentPage * limit}{' '}
-                av {numberOfEntries} oppgaver
-            </p>
-        )}
+    <div className={styles.PaginationContainer}>
+        <div className={styles.Pagination}>
+            <NavPagination
+                page={currentPage}
+                onPageChange={setPage}
+                count={numberOfPages}
+                siblingCount={2}
+                prevNextTexts
+            />
+            {numberOfEntries > 0 && (
+                <p>
+                    Viser {currentPage * limit - (limit - 1)} til{' '}
+                    {currentPage === numberOfPages && numberOfEntries % limit !== 0
+                        ? currentPage * limit - (limit - (numberOfEntries % limit))
+                        : currentPage * limit}{' '}
+                    av {numberOfEntries} oppgaver
+                </p>
+            )}
+        </div>
     </div>
 );
